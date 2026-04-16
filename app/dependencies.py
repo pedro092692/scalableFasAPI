b067867aeefc228236d017e622cf853cf01c48cd
pedro092692 <pedro092692@gmail.com>
2026-04-16 14:02:29 +0200

    feat: create dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,18 @@
 from sqlalchemy.orm import Session
+from app.database import LocalSession
 from app.repositories.user_repo import UserRepository
 from app.services.user_service import UserService
+from fastapi import Depends
+
+
+def get_db():
+    db = LocalSession()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    repo = UserRepository(db)
+    return UserService(repo)
